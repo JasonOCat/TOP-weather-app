ba0dbcf8ff17c3c62515c9ef02686b613f361207
Jason Catbagan <jcatbagan@crystal-holding.fr>
2024-01-21 20:22:57 +0100

    Initial commit structure webpack"

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/style.css';
+import './css/header.css';
+import './css/footer.css';
+import './css/middle-content.css';
+import './css/sidebar.css';
+
+console.log('hello world');
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+  mode,
+  entry: './src/index.js',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    watchFiles: ['src/**/*.html'],
+  },
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[name].[hash][ext][query]',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-object-rest-spread'],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  // devtool: 'source-map',
+};
